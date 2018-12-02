52064bfaf770aac7e6702dce4333a5aeab1b82e1
unknown <duanyiheng@gmail.com>
2018-12-03 01:54:57 +0800

    first commit at home

---
+++ src/main/java/stackAndArray/SortOneStackWithAnother.java
@@ -0,0 +1,35 @@
+package stackAndArray;
+
+import java.util.Stack;
+
+public class SortOneStackWithAnother {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SortOneStackWithAnother sortOneStackWithAnother = new SortOneStackWithAnother();
+        Stack<Integer> stack = new Stack<>();
+        stack.push(3);
+        stack.push(4);
+        stack.push(2);
+        stack.push(1);
+        System.out.println(stack);
+        sortOneStackWithAnother.sortByStack(stack);
+        System.out.println(stack);
+    }
+
+    public void sortByStack(Stack<Integer> data) {
+        if (null != data && !data.isEmpty()) {
+            Stack<Integer> sort = new Stack<>();
+            while (!data.isEmpty()) {
+                int num = data.pop();
+                while (!sort.isEmpty() && num > sort.peek()) {
+                    data.push(sort.pop());
+                }
+                sort.push(num);
+            }
+            while (!sort.isEmpty()) {
+                data.push(sort.pop());
+            }
+        }
+    }
+}
