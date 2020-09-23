bc3a1ac356e724d6f9f40d879c8681fbcedfcb80
unknown <duanyiheng@gmail.com>
2020-09-23 22:24:07 +0800

    ThreeSum

---
+++ src/main/java/leetcode/editor/en/ThreeSum.java
@@ -0,0 +1,84 @@
+package leetcode.editor.en;
+//Given an array nums of n integers, are there elements a, b, c in nums such tha
+//t a + b + c = 0? Find all unique triplets in the array which gives the sum of ze
+//ro. 
+//
+// Notice that the solution set must not contain duplicate triplets. 
+//
+// 
+// Example 1: 
+// Input: nums = [-1,0,1,2,-1,-4]
+//Output: [[-1,-1,2],[-1,0,1]]
+// Example 2: 
+// Input: nums = []
+//Output: []
+// Example 3: 
+// Input: nums = [0]
+//Output: []
+// 
+// 
+// Constraints: 
+//
+// 
+// 0 <= nums.length <= 3000 
+// -105 <= nums[i] <= 105 
+// 
+// Related Topics Array Two Pointers 
+// 👍 8015 👎 877
+
+import java.util.*;
+
+public class ThreeSum {
+    public static void main(String[] args) {
+        Solution solution = new ThreeSum().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> threeSum(int[] nums) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (nums != null && nums.length > 2) {
+                Arrays.sort(nums);
+                int n = nums.length;
+                List<Integer> val;
+                int lastIstValue = 0;
+                Set<List<Integer>> set = new HashSet<>();
+                for (int i = 0; i < n - 2; i++) {
+                    if (i > 0 && lastIstValue == nums[i]) {
+                        continue;
+                    }
+                    int j = i + 1;
+                    int k = n - 1;
+                    while (j < k) {
+                        int sum = nums[i] + nums[j] + nums[k];
+                        if (sum > 0) {
+                            k--;
+                        } else if (sum < 0) {
+                            j++;
+                        } else {
+                            val = new ArrayList<>();
+                            val.add(nums[i]);
+                            val.add(nums[j]);
+                            val.add(nums[k]);
+                            set.add(val);
+                            while (j < k && nums[j] == nums[j + 1]) {
+                                j++;
+                            }
+
+                            while (j < k && nums[k] == nums[k - 1]) {
+                                k--;
+                            }
+                            j++;
+                            k--;
+                        }
+                    }
+                    lastIstValue = nums[i];
+                }
+                res = new ArrayList<>(set);
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
