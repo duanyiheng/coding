f1e9e0c63ba6e2d89331ffdbce3879f4d5d88b4c
Yiheng Duan <duan_y@worksap.co.jp>
2020-09-25 11:47:34 +0800

    improve threeSum by trimming

--- src/main/java/leetcode/editor/en/ThreeSum.java
+++ src/main/java/leetcode/editor/en/ThreeSum.java
@@ -26,7 +26,11 @@ package leetcode.editor.en;
 // Related Topics Array Two Pointers 
 // 👍 8015 👎 877
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class ThreeSum {
     public static void main(String[] args) {
@@ -47,6 +51,12 @@ public class ThreeSum {
                     if (i > 0 && lastIstValue == nums[i]) {
                         continue;
                     }
+                    if (nums[i] + nums[i + 1] + nums[i + 2] > 0) {
+                        break;
+                    }
+                    if (nums[i] + nums[n - 2] + nums[n - 1] < 0) {
+                        continue;
+                    }
                     int j = i + 1;
                     int k = n - 1;
                     while (j < k) {
@@ -64,7 +74,6 @@ public class ThreeSum {
                             while (j < k && nums[j] == nums[j + 1]) {
                                 j++;
                             }
-
                             while (j < k && nums[k] == nums[k - 1]) {
                                 k--;
                             }
