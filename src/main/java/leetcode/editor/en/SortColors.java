6dd6fd6d7172c3d0d32e8947ff1e19b0be7e3e97
Yiheng Duan <duan_y@worksap.co.jp>
2020-11-03 17:01:22 +0800

    SortColors

---
+++ src/main/java/leetcode/editor/en/SortColors.java
@@ -0,0 +1,72 @@
+package leetcode.editor.en;
+//Given an array nums with n objects colored red, white, or blue, sort them in-p
+//lace so that objects of the same color are adjacent, with the colors in the orde
+//r red, white, and blue. 
+//
+// Here, we will use the integers 0, 1, and 2 to represent the color red, white,
+// and blue respectively. 
+//
+// Follow up: 
+//
+// 
+// Could you solve this problem without using the library's sort function? 
+// Could you come up with a one-pass algorithm using only O(1) constant space? 
+// 
+//
+// 
+// Example 1: 
+// Input: nums = [2,0,2,1,1,0]
+//Output: [0,0,1,1,2,2]
+// Example 2: 
+// Input: nums = [2,0,1]
+//Output: [0,1,2]
+// Example 3: 
+// Input: nums = [0]
+//Output: [0]
+// Example 4: 
+// Input: nums = [1]
+//Output: [1]
+// 
+// 
+// Constraints: 
+//
+// 
+// n == nums.length 
+// 1 <= n <= 300 
+// nums[i] is 0, 1, or 2. 
+// 
+// Related Topics Array Two Pointers Sort 
+// 👍 4265 👎 245
+
+public class SortColors {
+    public static void main(String[] args) {
+        Solution solution = new SortColors().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public void sortColors(int[] nums) {
+            int l = 0;
+            int i = 0;
+            int j = nums.length - 1;
+            while (i <= j) {
+                if (nums[i] == 0) {
+                    swap(nums, l++, i++);
+                } else if (nums[i] == 2) {
+                    swap(nums, i, j--);
+                } else {
+                    i++;
+                }
+            }
+        }
+
+        private void swap(int[] nums, int i, int j) {
+            int t = nums[i];
+            nums[i] = nums[j];
+            nums[j] = t;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
